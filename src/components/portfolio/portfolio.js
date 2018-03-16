b2eeec75bec6e7e79af5a5b004ad800536dc4f2e
Jesse Shaw <jdshaw1987@gmail.com>
2018-03-16 18:11:06 -0400

    Updated Portfolio. Project Sections

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -6,8 +6,10 @@ export default function Portfolio(props) {
     console.log(props.site)
     return (
         <section className={`portfolio ${props.projectname}`}>
-            <span className="site-identifier">This is portfolio site number {props.site}</span>
-            <img src={props.logo} alt="logo" className="project-logo"></img>
+            <section className="portfolio-section">
+                <span className="site-identifier">This is portfolio site number {props.site}</span>
+                <img src={props.logo} alt="logo" className="project-logo"></img>
+            </section>
         </section>
     );
 }
--- src/components/whoiam/whoiam.js
+++ src/components/whoiam/whoiam.js
@@ -5,7 +5,10 @@ import './whoiam.css'
 export default function Whoiam(props) {
     return (
         <section className="about-me">
-            "This is the section about me"
+            <section className="description">
+                <span className="aboutme-description">I am a web developer and recent graduate of the Front End Web Development Career Path bootcamp at Thinkful. I love coding because solving problems and seeing ideas come to life on the page is supremely satisfying. I geek out on higher order functions, finding and learning awesome libraries or frameworks, and getting front and back ends to play nicely.</span>
+                <span className="aboutme-description">When I'm not coding, I'm often found on Skype working with clients, curled up in a cozy chair reading, hiking in a canyon, or upside down doing a headstand. I am a lifelong learner and my current learning projects (beyond code!) include piano and writing my first book.</span>
+            </section>
         </section>
     );
 }
