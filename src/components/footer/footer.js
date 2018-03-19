884e7988599e3d8c33de8e7e077d11a8d28e5ba3
Jesse Shaw <jdshaw1987@gmail.com>
2018-03-19 10:44:16 -0400

    Updated Icon Settings. Starting to add font awesome. Set up project properties

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,12 @@ import './footer.css'
 export default function Footer(props) {
     return (
         <footer>
-            "This is the footer"
+            This is the Footer
+            <ul>
+            <li>
+                About
+            </li>
+            </ul>
         </footer>
     );
 }
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './portfolio.css'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
 
 export default function Portfolio(props) {
@@ -7,8 +8,14 @@ export default function Portfolio(props) {
     return (
         <section className={`portfolio ${props.projectname}`}>
             <section className="portfolio-section">
-                <span className="site-identifier">This is portfolio site number {props.site}</span>
+                <h1>{props.projectname}</h1>
+                <section>
                 <img src={props.logo} alt="logo" className="project-logo"></img>
+                </section>
+                <span className="site-identifier">{props.description}</span>
+                <section className="technolgy">
+                    {props.technologies}
+                </section>
             </section>
         </section>
     );
