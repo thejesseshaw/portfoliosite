0f2d0362c2f9f47d895734a9af3c0c08728c3445
Jesse Shaw <jdshaw1987@gmail.com>
2018-03-21 22:58:55 -0400

    Updated Media query of main

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -6,11 +6,11 @@ import splashImage from '../../images/helloquence-61189-unsplash.jpg'
 export default function Landing(props) {
     return (
         <section className="landing">
-            <section className="hero-image">
-                <img src={splashImage} alt="splashImage" className="splashImage"></img>
-            </section>
             <section className="greeting-section">
                 <span className="greeting">"Hi. I'm Jesse. I'm a fullstack developer"</span>
+            </section>
+            <section className="hero-image">
+                <img src={splashImage} alt="splashImage" className="splashImage"></img>
             </section>
         </section>
     );
