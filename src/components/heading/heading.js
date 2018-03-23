bc3c48efa7ed83dd3cb31bb18ad31d708e6fea2c
Jesse Shaw <jdshaw1987@gmail.com>
2018-03-22 21:43:39 -0400

    Various Style Updates

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -6,15 +6,17 @@ export default function Heading(props) {
     return (
         <header>
         <div className="container">
-            <section>
-                <h1>Jesse Shaw</h1>
+            <section className="name">
+                <a href="/"><h1 className="name-text" >Jesse Shaw</h1></a>
             </section>
+            <nav>
             <ul className="links">
             <li><a className="jumper" href="#bio" rel="noopener noreferrer">Bio</a></li>
             <li><a className="jumper" href="#portfolio" rel="noopener noreferrer">Portfolio</a></li>
             <li><a className="jumper" target="_blank" href="https://github.com/thejesseshaw" rel="noopener noreferrer">Github</a></li>
             <li><a className="jumper" target="_blank" href="https://www.linkedin.com/in/thejesseshaw/" rel="noopener noreferrer">LinkedIn</a></li>
             </ul>
+            </nav>
         </div>
         </header>
     );
